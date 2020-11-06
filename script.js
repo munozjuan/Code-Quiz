dfba8ff1b7b6dfee44ef612289614fe952527daa
munozjuan <munoz.juan07@hotmail.com>
2020-11-06 00:36:45 -0500

    added more questions

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 var quizBody = document.getElementById("quiz");
 var finalScoreEl = document.getElementById("finalScore");
 var gameoverDiv = document.getElementById("gameover");
+var resultsEl = document.getElementById("result");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreDiv = document.getElementById("high-scorePage");
+var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var endGameBtns = document.getElementById("endGameBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("highscore-score");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+
 
 
 //Code Quiz questions
@@ -11,4 +28,13 @@ var quizQuestions = [{
     choiceB: "Display Old Management",
     choiceC: "Digital Ordinance Model",
     choiceD: "Document Object Model",
-    correctAnswer: "a"}]
+    correctAnswer: "d"},
+        {
+        question: "What HTML tags are JavaScript code wrapped in?",
+    choiceA: "&lt;div&gt;",
+    choiceB: "&lt;link&gt;",
+    choiceC: "&lt;head&gt;",
+    choiceD: "&lt;script&gt;",
+    correctAnswer: "d"},
+
+]

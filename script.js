5ec8a90cfffcfc803ccef348c89f47d12fef5154
munozjuan <munoz.juan07@hotmail.com>
2020-11-06 00:18:41 -0500

    added vars

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-// Quiz question object
+// html vars 
+var quizBody = document.getElementById("quiz");
+var finalScoreEl = document.getElementById("finalScore");
+var gameoverDiv = document.getElementById("gameover");
 
 
-
+//Code Quiz questions
 var quizQuestions = [{
     question: "What does the acronym D.O.M. stand for?",
     choiceA: "Digital Object Model",
